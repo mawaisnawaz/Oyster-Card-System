ea67dbbe5c57eee348633c72f1d80dbe3eb05a1b
Muhammad Awais <awais00772@gmail.com>
2018-11-29 08:31:45 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+const Stations = require('./Modules/Stations');
+const Oyster = require('./Modules/Oyster');
+
+// Instantiate User:
+let oysterInstances = new Oyster();
+
+// Card charged with initial value 30
+oysterInstances.setCredit(30);
+
+/**  
+ *  Tube Holborn to Earl’s Court
+ */
+oysterInstances.enterTubeStation(new Stations('Holborn'));
+oysterInstances.exitTubeStation(new Stations("Earl's Court"));
+
+/**
+ * set bus trip from 328 bus from Earl’s Court to Chelsea
+* */ 
+oysterInstances.setNewBusJourney();
+
+/**  
+ *  Tube Earl’s court to Hammersmith
+ */
+oysterInstances.enterTubeStation(new Stations("Earl's Court"));
+oysterInstances.exitTubeStation(new Stations('Hammersmith'));
+
+var credit = oysterInstances.getCredit();
+console.log('Remaining Credit (£) :', credit);
